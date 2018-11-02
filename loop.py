e77918ce32cfbb1164a2c651acafd07d409d6164
PauIll <44305927+PauIll@users.noreply.github.com>
2018-11-02 17:47:25 +0100

    Update loop.py

--- loop.py
+++ loop.py
@@ -2,28 +2,18 @@ import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
 import asyncio
-import datetime
 import time
-from threading import *
-import os
 
 client = discord.Client()
 
-channeltime = '502399480176443394' #Channel où le Bot écrit le repère
 channelweb = '502510555618344960'   #Channel du webhook
 
 @client.event
 async def on_ready():
-    await client.change_presence(status=discord.Status.online, game= discord.Game(name="faire des galipettes", type=1))
-    print("The bot is ready!")
-
-async def my_background_task():
-    await client.wait_until_ready()
     while True:
         await client.purge_from(client.get_channel(channelweb), limit=10, check=None, before=None, after=None, around=None)
         client.send_message(await client.send_message(client.get_channel(channelweb),"!nbj"))
-        await asyncio.sleep(20)
-client.loop.create_task(my_background_task())
+        time.sleep(15)
 
 
 client.run(os.getenv('TOKEN'))
