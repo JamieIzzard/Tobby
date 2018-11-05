e79f41ca98a79c14877fd81d09bd4b194747f73a
PauIll <44305927+PauIll@users.noreply.github.com>
2018-11-05 13:14:51 +0100

    Update loop.py

--- loop.py
+++ loop.py
@@ -8,12 +8,25 @@ import os
 client = discord.Client()
 
 channelweb = '502510555618344960'   #Channel du webhook
+channel = '502399480176443394'      #Channel timer
 
 @client.event
 async def on_ready():
     while True:
         await client.purge_from(client.get_channel(channelweb), limit=10, check=None, before=None, after=None, around=None)
         client.send_message(await client.send_message(client.get_channel(channelweb),"!nbj"))
+        time.sleep(45)
+        await client.purge_from(client.get_channel(channelweb), limit=10, check=None, before=None, after=None, around=None)
+        client.send_message(await client.send_message(client.get_channel(channelweb),"!nbj"))
+        time.sleep(45)
+        await client.purge_from(client.get_channel(channelweb), limit=10, check=None, before=None, after=None, around=None)
+        client.send_message(await client.send_message(client.get_channel(channelweb),"!nbj"))
+        time.sleep(45)
+        await client.purge_from(client.get_channel(channelweb), limit=10, check=None, before=None, after=None, around=None)
+        client.send_message(await client.send_message(client.get_channel(channelweb),"!nbj"))
+        time.sleep(45)
+        await client.purge_from(client.get_channel(channel), limit=10, check=None, before=None, after=None, around=None)
+        client.send_message(await client.send_message(client.get_channel(channel),"!mp"))
         time.sleep(45)
 
 
