cc2daa6a402de850177e0f5c8f39676e88643a8c
PauIll <44305927+PauIll@users.noreply.github.com>
2018-11-12 11:23:26 +0100

    Update loop.py

--- loop.py
+++ loop.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
+import asyncio
 import time
 import os
 
@@ -11,7 +12,7 @@ channelweb = '502510555618344960'   #Channel du webhook
 channel = '502399480176443394'      #Channel timer
 
 @client.event
-def on_ready():
+async def on_ready():
     while True:
         client.send_message(await client.send_message(client.get_channel(channelweb),"!nbj"))
         time.sleep(45)
